b415fdf48038acfc166486e4b59e0fb08a8aa311
Haritch-6509650757 <haritch.utc@dome.tu.ac.th>
2024-10-27 00:30:21 +0700

    Add upload avatar

---
+++ client/pages/userprofile/UploadAvatar.js
@@ -0,0 +1,116 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import {Button, Form, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
+
+const UploadAvatar = ({ userId, token, username, avatarUrl, setisUserUpdated }) => {
+    const [modal, setModal] = useState(false);
+    const [file, setFile] = useState(null);
+
+    const toggle = () => {
+        setModal(!modal);
+    };
+
+    const handleFileChange = ({ target: { files }}) => {
+        if (files?.length){
+            const { type } = files[0];
+            if(type === "image/png" || type === "image/jpg"){
+                setFile(files[0]);
+            } else{
+                toast.error("Accept only png and jpeg image types",{
+                    hideprogessBar: true,
+                });
+                
+            }
+        }
+    };
+
+    const updateUserAvatarId = async (avatarId, avatarUrl) => {
+        try {
+            await axios.put(
+                `http://localhost:1337/api/users/${userId}`,
+                {avatarId, avatarUrl},
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `bearer ${token}`,
+                    },
+                }
+            );
+            setisUserUpdated(true);
+        } catch (error) {
+            console.log({ error });
+        }
+    };
+
+    const handleSubmit = async () => {
+        if (!file) {
+            toast.error("File is required", {
+                hideprogessBar: true,
+            });
+            return;
+        }
+
+        try {
+            const files = new FormData();
+            files.append("files", file);
+            files.append("name", `${username} avatar`);
+
+            const {
+                data: [{ id, url }],
+            } = await axios.post(`http://localhost:1337/api/upload`, files, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    Authorization: `bearer ${token}`,
+                },
+            });
+            updateUserAvatarId(id, url);
+            setFile(null);
+            setModal(false);
+
+        } catch (error) {
+            console.log({ error });
+        }
+    };
+
+    const[isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    return ( 
+    <div>
+        {isClient && (
+            <Button size="sm" onClick={toggle}>
+                {`${avatarUrl ? "Change" : "Upload"} picture`}
+            </Button>
+        )}
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>{`${avatarUrl ? "Change" : "Upload"} your picture`} </ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label for="exampleFile">File</Label>
+                            <Input
+                                type="file"
+                                name="file"
+                                id="exampleFile"
+                                onChange={handleFileChange}
+                            />
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={handleSubmit}>
+                        Upload
+                    </Button>{" "}
+                    <Button color="secondary" onClick={toggle}>
+                        Cancel
+                    </Button>
+                </ModalFooter>
+            </Modal>
+    </div>
+    );
+};
+
+export default UploadAvatar;
--- client/pages/userprofile/index.js
+++ client/pages/userprofile/index.js
@@ -1,12 +1,129 @@
-import React from "react";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import UploadAvatar from "./UploadAvatar";
 
-const UserProfile = () => {
-    return (
-        <div>
-            <h1>User Profile</h1>
-            <p>Test User Profile</p>
+const UserProfile = ({ token = "your token"}) => {
+   const [user, setUser] = useState({});
+   const [isUserUpdated , setisUserUpdated] = useState(false);
+    
+   
+   useEffect(() => {
+    const getProfileData = async () => {
+        try {
+            const {data} = await axios.get(`http://localhost:1337/api/users/me`, {
+                headers: {
+                    Authorization: `bearer ${token}`,
+                },
+            });
+            setUser(data);
+            setisUserUpdated(false);
+        } catch (error){
+            console.log({error});
+        }
+       };
+       getProfileData()
+   },[token, isUserUpdated])
+
+   console.log({user})
+   
+   return (
+        <div className="profile">
+            <div className="avatar">
+                <div className="avatar-wrapper">
+                    {user.avatarUrl ? (
+                        <img
+                            src={`http://localhost:1337${user.avatarUrl}`}
+                            alt={`${user.username} avatar`}
+                        />
+                    ) : (
+                        <IoPersonCircleOutline />
+                    )}
+                    <UploadAvatar
+                        token={token}
+                        userId={user.id}
+                        username={user.username}
+                        //email={user.email}
+                        //job={user.job}
+                        avatarUrl={user.avatarUrl}
+                        setisUserUpdated={setisUserUpdated}
+                    />
+                </div>
+            </div>
+            <div className="body">
+                <p>Name: {user.username}</p>
+                <p>Email: {user.email}</p>
+                <p>Account: Not blocked</p>
+            </div>
+
         </div>
     );
 };
 
 export default UserProfile;
+
+/*import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import UploadAvatar from "./UploadAvatar";
+
+const UserProfile = ({ token }) => {
+   const [user, setUser] = useState({
+       avatarUrl: '',
+       username: '',
+       email: '',
+       job: '',
+       id: null,
+   });
+   const [isUserUpdated, setIsUserUpdated] = useState(false);
+
+   useEffect(() => {
+       const getProfileData = async () => {
+           try {
+               const { data } = await axios.get(`http://localhost:1337/api/users/me`, {
+                   headers: {
+                       Authorization: `bearer ${token}`,
+                   },
+               });
+               setUser(data);
+               setIsUserUpdated(false);
+           } catch (error) {
+               console.log({ error });
+           }
+       };
+       getProfileData();
+   }, [token, isUserUpdated]);
+
+   return (
+       <div className="profile">
+           <div className="avatar">
+               <div className="avatar-wrapper">
+                   {user && user.avatarUrl ? (
+                       <img
+                           src={`http://localhost:1337${user.avatarUrl}`}
+                           alt={`${user.username} avatar`}
+                       />
+                   ) : (
+                       <IoPersonCircleOutline />
+                   )}
+                   <UploadAvatar
+                       token={token}
+                       userId={user.id}
+                       username={user.username}
+                       email={user.email}
+                       job={user.job}
+                       avatarUrl={user.avatarUrl}
+                       setisUserUpdated={setIsUserUpdated}
+                   />
+               </div>
+           </div>
+           <div className="body">
+               <p>Name: {user.username || "N/A"}</p>
+               <p>Email: {user.email || "N/A"}</p>
+               <p>Account: {user.isBlocked ? "Blocked" : "Not blocked"}</p>
+           </div>
+       </div>
+   );
+};
+
+export default UserProfile;*/
