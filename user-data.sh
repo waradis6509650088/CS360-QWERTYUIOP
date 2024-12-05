b63b1dcf9bcfb356c942017b26a89bf8f33220c1
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2024-12-05 22:18:10 +0700

    add docker-compose

--- user-data.sh
+++ user-data.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-# Update the package manager and install Docker
+# Update system
 sudo yum update -y
+
+# Install Docker
 sudo yum install -y docker
-
-# Start docker serivce
 sudo systemctl start docker
-
-# Add ec2-user to the docker group
+sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 
-# Apply group changes without logout
-newgrp docker
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-#create necessary directories for Strapi
-sudo mkdir -P /app/public/uploads
+# Create directory for uploads
+sudo mkdir -p /app/public/uploads
 sudo chmod -R 775 /app/public/uploads
+sudo chown -R ec2-user:ec2-user /app
 
-# Restart docker service to apply change
+# Restart Docker
 sudo systemctl restart docker
